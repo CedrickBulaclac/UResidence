8e655ed41ec234f766127ec7072829998caa9b0c
MartinPastera <36395393+MartinPastera@users.noreply.github.com>
2018-02-19 19:35:46 +0800

    Amenity Reservation

---
+++ UResidence/Controllers/AmenityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UResidence.Controllers
+{
+    public class AmenityController : Controller
+    {
+        // GET: Amenity
+        public ActionResult Reserve()
+        {
+            return View();
+        }
+    }
+}
