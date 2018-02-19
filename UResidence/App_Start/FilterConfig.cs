ab65db6bc4e502ce8ac0a1b0113c5d13e2c30a61
MartinPastera <36395393+MartinPastera@users.noreply.github.com>
2018-02-19 19:19:09 +0800

    sdsdsdsd

---
+++ UResidence/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace UResidence
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
