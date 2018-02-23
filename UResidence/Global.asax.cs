411752f1faa15c587b300c8bfc9e130dfc7c2316
CedrickBulaclac <cedrickbulaclac0903@gmail.com>
2018-02-23 20:09:10 +0800

    new comment
    new alsdkfjlasjfd;l

--- UResidence/Global.asax.cs
+++ UResidence/Global.asax.cs
@@ -10,6 +10,7 @@ namespace UResidence
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        //hala tuwang tuwa
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
