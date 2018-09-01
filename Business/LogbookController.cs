d96604afad7329b33cd0348f3d40904e2fed77e7
CedrickBulaclac <cedrickbulaclac0903@gmail.com>
2018-09-02 02:39:15 +0800

    Entrance/Exit
    Viewing

---
+++ Business/LogbookController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace UResidence
+{
+    public class LogbookController
+    {
+        public static List<Logbook> GET_ALL()
+        {
+            const string GET_ALL = @"select Id,Date,VisitorName,ResidentName,TimeIn,TimeOut,Purpose from [tbLogbook]";
+            List<Logbook> ret = default(List<Logbook>);
+            SqlCommand com = new SqlCommand(GET_ALL);
+            ret = SqlManager.Select<Logbook>(com);
+            return ret;
+
+        }
+    }
+}
---
+++ UResidence/Controllers/LogBookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UResidence.Controllers
+{
+    public class LogBookController : Controller
+    {
+        // GET: LogBook
+        public ActionResult LogBook()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            List<Logbook> log = new List<Logbook>();
+            log = UResidence.LogbookController.GET_ALL();
+            var events = log.ToList();
+            return new JsonResult
+            {
+                Data = events,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+    }
+}
