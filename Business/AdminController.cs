33bcbd2d027fb9fa7e8ba3b9a502ed4d6b43a36f
CedrickBulaclac <cedrickbulaclac0903@gmail.com>
2018-09-22 02:56:45 +0800

    Birthday add field select

--- Business/AdminController.cs
+++ Business/AdminController.cs
@@ -11,7 +11,7 @@ namespace UResidence
     {
         public static List<Admin> GetAll()
         {
-            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and Level!=0 order by Role asc";
+            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and Level!=0 order by Role asc";
             List<Admin> ret = default(List<Admin>);
             SqlCommand com = new SqlCommand(GET_ALL);
             ret = SqlManager.Select<Admin>(com);
@@ -19,7 +19,7 @@ namespace UResidence
         }
         public static List<Admin> GetAllCashier()
         {
-            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role,Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and Level=3 order by Role asc";
+            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role,Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and Level=3 order by Role asc";
             List<Admin> ret = default(List<Admin>);
             SqlCommand com = new SqlCommand(GET_ALL);
             ret = SqlManager.Select<Admin>(com);
@@ -27,7 +27,7 @@ namespace UResidence
         }
         public static List<Admin> GetAllSecurity()
         {
-            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and Level=7 order by Role asc";
+            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and Level=7 order by Role asc";
             List<Admin> ret = default(List<Admin>);
             SqlCommand com = new SqlCommand(GET_ALL);
             ret = SqlManager.Select<Admin>(com);
@@ -35,7 +35,7 @@ namespace UResidence
         }
         public static List<Admin> Get(int id)
         {
-            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and a.Id=@Rid and Level!=0 order by Role asc";
+            const string GET_ALL = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where Deleted=0 and a.Id=@Rid and Level!=0 order by Role asc";
             List<Admin> ret = default(List<Admin>);
             SqlCommand com = new SqlCommand(GET_ALL);
             com.Parameters.Add(new SqlParameter("@Rid", id));
@@ -44,7 +44,7 @@ namespace UResidence
         }
         public static Admin GetbyID(int Id)
         {
-            const string GET_RECORD = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where a.Id = @Id order by Role asc";
+            const string GET_RECORD = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where a.Id = @Id order by Role asc";
            Admin ret = default(Admin);
             SqlCommand com = new SqlCommand(GET_RECORD);
             com.Parameters.Add(new SqlParameter("@Id", Id));
@@ -55,7 +55,7 @@ namespace UResidence
 
         public static Admin GetIdAdmin(string idAdmin)
         {
-            const string GET_RECORD = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where a.Id =@Id  order by Role asc";
+            const string GET_RECORD = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where a.Id =@Id  order by Role asc";
             Admin ret = default(Admin);
             SqlCommand com = new SqlCommand(GET_RECORD);
             com.Parameters.Add(new SqlParameter("@Id", idAdmin));
@@ -66,7 +66,7 @@ namespace UResidence
 
         public static Admin GetbyIDEdit(string Id)
         {
-            const string GET_RECORD = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role , Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where a.Id=@Id and Level!=0 order by Role asc";
+            const string GET_RECORD = @"SELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role , Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id where a.Id=@Id and Level!=0 order by Role asc";
             Admin ret = default(Admin);
             SqlCommand com = new SqlCommand(GET_RECORD);
             com.Parameters.Add(new SqlParameter("@Id", Id));
@@ -78,7 +78,7 @@ namespace UResidence
 
         public static Admin GetEmailAdmin(string email)
         {
-            const string GET_RECORD = @"SSELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id WHERE Email = @Email order by Role asc";
+            const string GET_RECORD = @"SSELECT a.Id,Fname,Mname,Lname,Bdate,CelNo,Email,Deleted,Url,case Level when 0 then 'Super Admin'  when 1 then 'Manager' when 2 then 'Finance' when 3 then 'Cashier' when 4 then 'Reservation Admin' when 5 then 'Registration' when 6 then 'OIC Security Guard' when 7 then 'Security Guard' end as Role, Fname+' '+Mname+' '+Lname as Fullname,FORMAT(Bdate,'MMM dd yyyy') as Birthday FROM [tbAdmin] a inner join tbLogin l on l.AdminId=a.Id WHERE Email = @Email order by Role asc";
            Admin ret = default(Admin);
             SqlCommand com = new SqlCommand(GET_RECORD);
             com.Parameters.Add(new SqlParameter("@Email", email));
