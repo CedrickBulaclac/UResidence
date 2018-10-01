c9481e6e9af312f4710e35f68ccbbb8b0e03d565
CedrickBulaclac <cedrickbulaclac0903@gmail.com>
2018-10-01 19:04:07 +0800

    Account Settings Manager

--- UResidence/Controllers/AccountSettingController.cs
+++ UResidence/Controllers/AccountSettingController.cs
@@ -20,12 +20,83 @@ namespace UResidence.Controllers
                 List<Admin> a = new List<Admin>();
                 List<UserLogin> ul = new List<UserLogin>();
                 
-                a=UResidence.AdminController.Get(Aid);
+                a =UResidence.AdminController.Get(Aid);
                 ul=UResidence.UserController.AGetAll(Aid);
                 List<object> model = new List<object>();
                 model.Add(a.ToList());
                 model.Add(ul.ToList());
+        
+                return View(model);
+            
+        }
+        public ActionResult ManagerAccountSetting()
+        {
+            string pass = (string)Session["pass"];
+            string RType = (string)Session["TOR"];
+            int Aid = (int)Session["UID"];
+            List<Admin> a = new List<Admin>();
+            List<UserLogin> ul = new List<UserLogin>();
+            a = UResidence.AdminController.Get(Aid);
+            ul = UResidence.UserController.AGetAll(Aid);
+            List<object> model = new List<object>();
+            model.Add(a.ToList());
+            model.Add(ul.ToList());
             return View(model);
+        }
+        [HttpPost]
+        public ActionResult ManagerAccountSetting(FormCollection fc)
+        {
+            int lid = (int)Session["LID"];
+            bool status = false;
+            int Aid = (int)Session["UID"];
+            string pass = (string)Session["pass"];
+            string cp = fc["cp"];
+            string Fname = fc["fname"];
+            string Mname = fc["mname"];
+            string Lname = fc["lname"];
+            string np = fc["np"];
+            string npass = Hash(np);
+            string username = fc["username"];
+            string tor = (string)Session["TOR"];
+            int locked = 0;
+            if (pass == cp)
+            {
+                Admin a = new Admin
+                {
+                    Id = Aid,
+                    Fname = Fname,
+                    Mname = Mname,
+                    Lname = Lname,
+
+                };
+                status = UResidence.AdminController.AUpdate(a);
+                if (status == true)
+                {
+                    UserLogin ul = new UserLogin
+                    {
+                        Id = lid,
+                        Username = username,
+                        Hash = npass,
+                        ModifyBy = tor,
+                        Locked = locked,
+                    };
+                    status = UResidence.UserController.Update(ul);
+                    if (status == true)
+                    {
+
+                        return RedirectToAction("Home", "Admin");
+                    }
+
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('Current Password is incorrect')</script>");
+
+            }
+            return AdminAccountSetting();
+
+
         }
         [HttpPost]
         public ActionResult AdminAccountSetting(FormCollection fc)
